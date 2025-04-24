8c870b1d707a17242a298c7cc45d0299ef2005b3
vanoss22 <leeweixuan39@gmail.com>
2025-04-25 05:32:18 +0800

    feat: setup connection to Quorum Node0

--- src/test/java/com/usfbs/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/usfbs/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,25 @@
 package com.usfbs.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.quorum.Quorum;
+
+import com.usfbs.springboot.service.QuorumService;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testConnectionToNode() throws Exception {
+		QuorumService service = new QuorumService();
+		Quorum quorum = service.getQuorum();
+		Web3ClientVersion version = quorum.web3ClientVersion().send();
+
+		assertNotNull(version.getWeb3ClientVersion());
+		System.out.println("Client Version: " + version.getWeb3ClientVersion());
 	}
 
 }
