c236ab2ead402a9ea4a66fb9b7fe701855a0436a
vanoss22 <leeweixuan39@gmail.com>
2025-04-27 17:53:45 +0800

    chore: config boilerplate

---
+++ src/main/java/com/usfbs/springboot/configuration/QuorumConfig.java
@@ -0,0 +1,44 @@
+package com.usfbs.springboot.configuration;
+
+import java.math.BigInteger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.quorum.Quorum;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
+
+@Configuration
+public class QuorumConfig {
+    @Value("${quorum.url}")
+    private String quorumUrl;
+    @Value("${quorum.privateKey}")
+    private String privateKey;
+
+    @Bean
+    public Quorum quorum() {
+        return Quorum.build(new HttpService(quorumUrl));
+    }
+
+    @Bean
+    public Credentials credentials() {
+        return Credentials.create(privateKey);
+    }
+
+    @Bean
+    public ContractGasProvider contractGasProvider() {
+        return new StaticGasProvider(BigInteger.ZERO, BigInteger.valueOf(4700000));
+    }
+
+    // @Bean
+    // TODO: Booking contract
+
+    // @Bean
+    // TODO: Facility contract
+
+    // @Bean
+    // TODO: Management contract
+}
