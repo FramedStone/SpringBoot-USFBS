a1bb532e36f121cc67b7cb51d475800359a101a8
vanoss22 <leeweixuan39@gmail.com>
2025-05-25 21:33:35 +0800

    feat: remove metamask option.

--- frontend/src/components/web3authContext.jsx
+++ frontend/src/components/web3authContext.jsx
@@ -1,4 +1,4 @@
-import { WEB3AUTH_NETWORK } from "@web3auth/modal";
+import { WEB3AUTH_NETWORK, WALLET_CONNECTORS } from "@web3auth/modal";
 import { CHAIN_NAMESPACES }  from "@web3auth/base";
 
 const CLIENT_ID = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
@@ -9,6 +9,12 @@ const web3AuthContextConfig = {
   web3AuthOptions: {
     clientId: CLIENT_ID,
     web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET,  
+    modalConfig: {
+      connectors: {
+        [WALLET_CONNECTORS.METAMASK] : { showOnModal: false },
+        [WALLET_CONNECTORS.AUTH] : { showOnModal: true }
+      },
+    },
   },
   chainConfig: {                               
     chainNamespace: CHAIN_NAMESPACES.EIP155,
