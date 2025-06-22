db25bd46cec9dc591a57cb9e81b34e2b79b83e9c
vanoss22 <leeweixuan39@gmail.com>
2025-06-22 18:49:28 +0800

    toString for address

--- backend/src/main/resources/contracts/Strings.sol
+++ backend/src/main/resources/contracts/Strings.sol
@@ -22,6 +22,27 @@ library Strings {
         return string(buffer);
     }
 
+    // Converts an `address` to its ASCII `string` hexadecimal representation.
+    function toString(address addr) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(addr)), 20);
+    }
+
+    // Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
+    }
+
+    // Hexadecimal character set
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
     // Converts a `uint256` to 24 hours time format
     function uintTo24Hour(uint256 secs) internal pure returns (string memory) {
         uint256 hh = secs / 3600;
